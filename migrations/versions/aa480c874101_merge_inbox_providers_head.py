d777ea079d44d0827b68618203f2b7fbaebae82d
matat-server <ops@matatmordechai.org>
2026-05-01 09:05:07 +0000

    merge: resolve concurrent migration heads

---
+++ migrations/versions/aa480c874101_merge_inbox_providers_head.py
@@ -0,0 +1,24 @@
+"""merge inbox providers head
+
+Revision ID: aa480c874101
+Revises: c3d4e5f6a7b8, 8a98b930f565
+Create Date: 2026-05-01 09:04:51.218056
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'aa480c874101'
+down_revision = ('c3d4e5f6a7b8', '8a98b930f565')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
